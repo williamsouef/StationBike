75a1533815d111ec58b67a28b76311c352ede559
William Souef <williamsouef@gmail.com>
2023-10-17 08:53:19 +0200

    Initial Commit

---
+++ StationBike/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StationBike
+//
+//  Created by William Souef on 17/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
